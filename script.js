b54d0a56b64b0bcdede5f88b5f2750b21a3a202a
Arpit Sinha <arpitsinha579@gmail.com>
2024-12-03 22:45:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+//Object that stores values of minimum and maximum angle for a value
+const rotationValues = [
+  { minDegree: 0, maxDegree: 30, value: "better luck next time" },
+  { minDegree: 31, maxDegree: 90, value: "you won the chocolate" },
+  { minDegree: 91, maxDegree: 150, value: "better luck next time" },
+  { minDegree: 151, maxDegree: 210, value: "better luck next time" },
+  { minDegree: 211, maxDegree: 270, value: 'better luck next time' },
+  { minDegree: 271, maxDegree: 330, value: "you won the chocolate" },
+  { minDegree: 331, maxDegree: 360, value: "you won the chocolate" },
+];
+//Size of each piece
+const data = [16, 16, 16, 16, 16, 16];
+//background color for each piece
+var pieColors = [
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+];
+//Create chart
+let myChart = new Chart(wheel, {
+  //Plugin for displaying text on pie chart
+  plugins: [ChartDataLabels],
+  //Chart Type Pie
+  type: "pie",
+  data: {
+    //Labels(values which are to be displayed on chart)
+    labels: [1, 2, 3, 4, 5, 6],
+    //Settings for dataset/pie
+    datasets: [
+      {
+        backgroundColor: pieColors,
+        data: data,
+      },
+    ],
+  },
+  options: {
+    //Responsive chart
+    responsive: true,
+    animation: { duration: 0 },
+    plugins: {
+      //hide tooltip and legend
+      tooltip: false,
+      legend: {
+        display: false,
+      },
+      //display labels inside pie chart
+      datalabels: {
+        color: "#ffffff",
+        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+        font: { size: 24 },
+      },
+    },
+  },
+});
+//display value based on the randomAngle
+const valueGenerator = (angleValue) => {
+  for (let i of rotationValues) {
+    //if the angleValue is between min and max then display it
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+      spinBtn.disabled = false;
+      break;
+    }
+  }
+};
